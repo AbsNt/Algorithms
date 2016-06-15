abd25f8d4eaf1d455f9eab33135848f3b9c21df5
Absnt <simsoid96@gmail.com>
2016-06-15 11:26:18 +1000

    CreateConflict

--- Factorial.pas
+++ Factorial.pas
@@ -1,13 +1,13 @@
 function fac(n:integer):longint;
 begin
-if n=0 then fac:=1
-else
-fac:=n*fac(n-1);
+  if n=0 then fac:=1
+  else
+  fac:=n*fac(n-1);
 end;
  
 var k:integer;
 begin
-writeln('Enter number ');
-readln(k);
-writeln('Factorial from ',k,' is ',fac(k));
+  writeln('Enter number ');
+  readln(k);
+  writeln('Factorial from ',k,' is ',fac(k));
 end.
